3c98f274ba72258790e5c9f4464e298fc8681209
Julio Gonzalez <jrgonzalezrios@gmail.com>
2015-12-02 19:57:17 -0600

    First commit basic config

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+	path = require('path'),
+	config = require('./config/config.js');
+
+
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('hogan-express'));
+
+app.set('view engine', 'html');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('port', process.env.PORT || 3000);
+app.set('host', config.host);
+
+require('./routes/routes.js')(express, app)
+
+var server = require('http').createServer(app);
+var io  = require('socket.io')(server);
+
+server.listen(app.get('port'), function(){
+	console.log('Photogrid running on port:' + app.get('port'));
+})
