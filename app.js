03d6ffcb294d8875a6e87a3fb3ea389c9ee19a70
Julio Gonzalez <jrgonzalezrios@gmail.com>
2015-12-08 22:04:36 -0600

    Adding support tu upload images to s3 and mongo

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express'),
 	path = require('path'),
-	config = require('./config/config.js');
+	config = require('./config/config.js'),
+	knox = require('knox'),
+	fs = require('fs'),
+	os = require('os'),
+	formidable = require('formidable'),
+	gm = require('gm'),
+	mongoose = require('mongoose').connect(config.dbURL);
 
 
 var app = express();
@@ -14,10 +20,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', process.env.PORT || 3000);
 app.set('host', config.host);
 
-require('./routes/routes.js')(express, app)
+// Knox client
+var knoxClient = knox.createClient({
+	key: config.S3AccessKey,
+	secret: config.S3Secret,
+	bucket: config.S3Bucket
+})
 
 var server = require('http').createServer(app);
 var io  = require('socket.io')(server);
+
+require('./routes/routes.js')(express, app, formidable, fs, os, gm, knoxClient, mongoose, io);
 
 server.listen(app.get('port'), function(){
 	console.log('Photogrid running on port:' + app.get('port'));
